b4b85d7db21beed372fd42c52ef75cb0ea939b57
Alex <ambulajan@gmail.com>
2017-04-22 21:19:35 +0300

    packages added to the load list

--- init.el
+++ init.el
@@ -14,13 +14,15 @@
     ;;; helm deps
     (helm-core t) (popup t) (async t) (helm t)
     (web-mode t)
-    (web-beautify t) 
+    (web-beautify t) ; requires "js-beautify" in npm 
     (php-mode t)
     (highlight-symbol t)
     (auto-complete t)
     (paredit t)
     (epc t)
     (ggtags t)
+	;;; dired
+    (dired-toggle-sudo t) (dired+ t) (dired-atool)
     ;;; jedi deps
     (python-environment t) (ctable t) (deferred t) (concurrent t) (jedi-core t) (jedi t)
     (bash-completion t) ; for shell mode
@@ -99,6 +101,14 @@
 (global-set-key (kbd "C-c h") 'helm-mini)
 (helm-mode 1)
 
+;;; dired
+(define-key dired-mode-map (kbd "C-c C-s") 'dired-toggle-sudo)
+;; (eval-after-load 'tramp
+;;  '(progn
+;;     ;; Allow to use: /sudo:user@host:/path/to/file
+;;     (add-to-list 'tramp-default-proxies-alist
+;; 		  '(".*" "\\`.+\\'" "/ssh:%h:"))))
+
 ;; something from stackoverflow
 (setq tramp-ssh-controlmaster-options
       "-o ControlMaster=auto -o ControlPath='tramp.%%C' -o ControlPersist=no")
@@ -114,20 +124,20 @@
 ;; disable semantic in some modes
 (setq semantic-new-buffer-setup-functions
       '((c-mode . semantic-default-c-setup)
-	(c++-mode . semantic-default-c-setup)
-	(html-mode . semantic-default-html-setup)
-	(java-mode . wisent-java-default-setup)
-	;;(js-mode . wisent-javascript-setup-parser) ; js-mode's imenu is better
-	(python-mode . wisent-python-default-setup)
-	(scheme-mode . semantic-default-scheme-setup)
-	(srecode-template-mode . srecode-template-setup-parser)
-	(texinfo-mode . semantic-default-texi-setup)
-	(makefile-automake-mode . semantic-default-make-setup)
-	(makefile-gmake-mode . semantic-default-make-setup)
-	(makefile-makepp-mode . semantic-default-make-setup)
-	(makefile-bsdmake-mode . semantic-default-make-setup)
-	(makefile-imake-mode . semantic-default-make-setup)
-	(makefile-mode . semantic-default-make-setup)))
+		(c++-mode . semantic-default-c-setup)
+		(html-mode . semantic-default-html-setup)
+		(java-mode . wisent-java-default-setup)
+		;;(js-mode . wisent-javascript-setup-parser) ; js-mode's imenu is better
+		(python-mode . wisent-python-default-setup)
+		(scheme-mode . semantic-default-scheme-setup)
+		(srecode-template-mode . srecode-template-setup-parser)
+		(texinfo-mode . semantic-default-texi-setup)
+		(makefile-automake-mode . semantic-default-make-setup)
+		(makefile-gmake-mode . semantic-default-make-setup)
+		(makefile-makepp-mode . semantic-default-make-setup)
+		(makefile-bsdmake-mode . semantic-default-make-setup)
+		(makefile-imake-mode . semantic-default-make-setup)
+		(makefile-mode . semantic-default-make-setup)))
 (add-hook 'semantic-inhibit-functions (lambda () (member major-mode '(js-mode))))
 (add-to-list 'semantic-default-submodes 'global-semantic-stickyfunc-mode)
 (add-to-list 'semantic-default-submodes 'global-semantic-decoration-mode)
@@ -165,6 +175,8 @@
 (defun python-mode-func ()
   (setq tab-width 4)
   (hs-minor-mode)
+  (jedi:setup)
+  ;;(enable-paredit-mode)
   (abbrev-mode 1)
   (define-abbrev python-mode-abbrev-table  "pdb" "import pdb;pdb.set_trace()")
   (define-abbrev python-mode-abbrev-table  "here" "raise Exception('here')")
@@ -177,9 +189,7 @@
   (local-set-key (kbd "C-c f") 'ak-flycheck-mode)
   ;;(define-key jedi-mode-map (kbd "C-c ,") nil) ; unhide Semantic
   (define-key jedi-mode-map (kbd "M-.") 'jedi:goto-definition)
-  (define-key jedi-mode-map (kbd "C-c p") 'jedi:goto-definition-pop-marker)
-  ;;(enable-paredit-mode)
-  (jedi:setup))
+  (define-key jedi-mode-map (kbd "C-c p") 'jedi:goto-definition-pop-marker))
 (add-hook 'python-mode-hook 'python-mode-func)
 
 ;;----------------------------------------------------
@@ -222,14 +232,14 @@
      (define-abbrev php-mode-abbrev-table  "vdb" "var_dump( debug_backtrace() );die();")
      ))
 (add-hook 'php-mode-hook (lambda ()
-			   (ggtags-mode 1)
-			   (local-unset-key "C-M-\\")
-			   (local-unset-key "C")
-			   (setq indent-tabs-mode nil
-				 tab-width 4
-				 c-basic-offset 4)
-			   ;;(setq-local imenu-create-index-function #'ggtags-build-imenu-index)
-			   ))
+						   (ggtags-mode 1)
+						   (local-unset-key "C-M-\\")
+						   (local-unset-key "C")
+						   (setq indent-tabs-mode nil
+								 tab-width 4
+								 c-basic-offset 4)
+						   ;;(setq-local imenu-create-index-function #'ggtags-build-imenu-index)
+						   ))
 (global-set-key (kbd "<f11>") 'php-mode)
 (add-to-list 'auto-mode-alist '("\\.php$"  . php-mode))
 
@@ -245,23 +255,23 @@
 (setq web-mode-engines-alist
       '(("django" . "\\.html\\'")))
 (add-hook 'web-mode-hook
-	  (lambda ()
-	    (define-abbrev web-mode-abbrev-table  "vdd" "var_dump(  );die();")
-	    (define-abbrev web-mode-abbrev-table  "cnl" "console.log();") ;
-	    (setq web-mode-enable-part-face t)
-	    (abbrev-mode 1)
-	    (setq web-mode-enable-current-element-highlight t)
-	    (set-face-attribute 'web-mode-current-element-highlight-face nil :background "#CCCCCC")
-	    (set-face-attribute 'web-mode-html-tag-face nil :foreground "#0000CD")
-	    (set-face-attribute 'web-mode-html-attr-name-face nil :foreground "#007700")
-	    (setq web-mode-enable-block-face t)
-	    (set-face-attribute 'web-mode-block-face nil :background "#E3F2E1")
-	    ))
+		  (lambda ()
+			(define-abbrev web-mode-abbrev-table  "vdd" "var_dump(  );die();")
+			(define-abbrev web-mode-abbrev-table  "cnl" "console.log();") ;
+			(setq web-mode-enable-part-face t)
+			(abbrev-mode 1)
+			(setq web-mode-enable-current-element-highlight t)
+			(set-face-attribute 'web-mode-current-element-highlight-face nil :background "#CCCCCC")
+			(set-face-attribute 'web-mode-html-tag-face nil :foreground "#0000CD")
+			(set-face-attribute 'web-mode-html-attr-name-face nil :foreground "#007700")
+			(setq web-mode-enable-block-face t)
+			(set-face-attribute 'web-mode-block-face nil :background "#E3F2E1")
+			))
 (setq web-mode-extra-snippets
       '((nil . (("div" . ("<div class=\"\">" . "</div>"))
-		;;("name" . ("beg" . "end"))
+				;;("name" . ("beg" . "end"))
+				))
 		))
-	))
 (global-set-key (kbd "<f12>") 'web-mode)
 
 ;;----------------------------------------------------
@@ -292,22 +302,22 @@
      (define-abbrev js-mode-abbrev-table  "cnl" "console.log(  );")))
 
 (add-hook 'js-mode-hook
-	  #'(lambda ()
-	      (setq tab-width 4)
-	      (abbrev-mode 1)
-	      (ggtags-mode 1)
-	      ;;(setq imenu-create-index-function #'ggtags-build-imenu-index)
-	      ;;(setq imenu-create-index-function #'js--imenu-create-index)
-	      (define-key js-mode-map (kbd "C-c b") 'web-beautify-js)
-	      (define-key ggtags-mode-map (kbd "M-.") 'ggtags-find-definition)
-	      (define-key ggtags-mode-map (kbd "M-n") 'idomenu)))
+		  #'(lambda ()
+			  (setq tab-width 4)
+			  (abbrev-mode 1)
+			  (ggtags-mode 1)
+			  ;;(setq imenu-create-index-function #'ggtags-build-imenu-index)
+			  ;;(setq imenu-create-index-function #'js--imenu-create-index)
+			  (define-key js-mode-map (kbd "C-c b") 'web-beautify-js)
+			  (define-key ggtags-mode-map (kbd "M-.") 'ggtags-find-definition)
+			  (define-key ggtags-mode-map (kbd "M-n") 'idomenu)))
 
 ;; flymake jslint
 ;;(add-hook 'js2-mode-hook 'flymake-jslint-load)
 
 (add-hook 'js-mode-hook
-	  #'(lambda ()
-	      (setq js-indent-level 2)))
+		  #'(lambda ()
+			  (setq js-indent-level 2)))
 
 (global-set-key (kbd "C-c C-r") 'revert-buffer)
 
@@ -317,14 +327,14 @@
   (concat (file-name-directory load-file-name) "prepaint/prepaint"))
 
 (add-hook 'c-mode-common-hook
-	  (lambda ()
-	    (ggtags-mode)
-	    (prepaint-mode 1)
-	    (setq c-macro-preprocessor "cpp -CC")
-	    ;;(setq-local imenu-create-index-function #'ggtags-build-imenu-index)
-	    ;;(flycheck-mode)
-	    (hs-minor-mode)
-	    (define-key c-mode-map "\C-c\C-f" 'ff-find-other-file)
+		  (lambda ()
+			(ggtags-mode)
+			(prepaint-mode 1)
+			(setq c-macro-preprocessor "cpp -CC")
+			;;(setq-local imenu-create-index-function #'ggtags-build-imenu-index)
+			;;(flycheck-mode)
+			(hs-minor-mode)
+			(define-key c-mode-map "\C-c\C-f" 'ff-find-other-file)
             (define-key c++-mode-map "\C-c\C-f" 'ff-find-other-file)))
 
 ;;****************************************************************
